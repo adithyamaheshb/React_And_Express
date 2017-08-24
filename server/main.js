f7726d81de3d95a7a447edc7d81bede5d86551a5
Adithya Mahesh Bariki <adithyamaheshbariki@gmail.com>
2017-08-24 00:20:37 -0500

    Scaffolding the App

---
+++ server/main.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = new express();
+
+app.use(express.static(__dirname + '/../.tmp'));
+
+app.get('/', function(req,res){
+    res.render('./../app/index.ejs',{});
+});
+
+app.listen(7777);
